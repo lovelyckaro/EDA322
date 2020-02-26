73357d70f5afe24e32095110648f078e5958ffca
William Risne <risne99@gmail.com>
2020-02-26 10:39:42 +0100

    Add memory array component

--- mem_array.vhd
+++ mem_array.vhd
@@ -1,5 +1,11 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.ALL;
+use std.textio.ALL;
+
+
+
+
 
 entity mem_array 
   is generic (  
@@ -16,10 +22,34 @@ entity mem_array
   );
 end mem_array;
 
-architecture bajs of mem_array is
-Type MEMORY_ARRAY is ARRAY (ADDR_WIDTH - 1 downto 0) of std_logic_vector(DATA_WIDTH-1 downto 0);
+
+
+architecture behavioral of mem_array is
+Type MEMORY_ARRAY is ARRAY ((2**ADDR_WIDTH) - 1 downto 0) of std_logic_vector(DATA_WIDTH-1 downto 0);
+impure function init_memory_wfile(mif_file_name : in string) return 
+MEMORY_ARRAY is
+   file mif_file : text open read_mode is mif_file_name;
+   variable mif_line : line;variable temp_bv : bit_vector(DATA_WIDTH-1 downto 0);
+   variable temp_mem : MEMORY_ARRAY;
+begin
+   for i in MEMORY_ARRAY'range loop
+      readline(mif_file, mif_line);
+      read(mif_line, temp_bv);
+      temp_mem(i) := to_stdlogicvector(temp_bv);
+   end loop;
+   return temp_mem;
+end function;
+
 signal memory : MEMORY_ARRAY := init_memory_wfile(INIT_FILE);
 
 begin
+  process(clk)
+  begin
+    if (rising_edge(clk) and WE = '1') then
+      memory(to_integer(unsigned(ADDR))) <= DATAIN;
+    elsif (rising_edge(clk) and WE = '0') then
+      OUTPUT <= memory(to_integer(unsigned(ADDR)));
+    end if;
+  end process;
 
-end bajs;
+end behavioral;
